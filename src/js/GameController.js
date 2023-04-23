b5acdd628a1050e6b018a6bcae859f4184cd54ae
Olesya Vetoshkina <our_post-office@mail.ru>
2023-04-23 04:47:07 +0300

    second commit

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -19,14 +19,14 @@ export default class GameController {
 
     this.goodTeam = goodTeam.toArrayOfPlayers().map((character, index) => new PositionedCharacter(
       character,
-      index === this.generateRandomPosition([
+      this.generateRandomPosition([
         0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57,
       ]),
     ));
 
     this.badTeam = badTeam.toArrayOfPlayers().map((character, index) => new PositionedCharacter(
       character,
-      index === this.generateRandomPosition([
+      this.generateRandomPosition([
         6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63,
       ]),
     ));
